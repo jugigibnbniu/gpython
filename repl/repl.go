cff5723c842bba2f087a8ea1da905283df74cf98
jugigibnbniu <130581635+jugigibnbniu@users.noreply.github.com>
2024-12-05 12:02:02 -0600

    Add files via upload

---
+++ repl/repl.go
@@ -0,0 +1,143 @@
+// Copyright 2018 The go-python Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Read Eval Print Loop
+package repl
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/jugigibnbniu/gpython/py"
+	"github.com/jugigibnbniu/gpython/vm"
+)
+
+// Possible prompts for the REPL
+const (
+	NormalPrompt       = ">>> "
+	ContinuationPrompt = "... "
+)
+
+// Repl state
+type REPL struct {
+	Context      py.Context
+	Module       *py.Module
+	prog         string
+	continuation bool
+	previous     string
+	term         UI
+}
+
+// UI implements the user interface for the REPL
+type UI interface {
+	// Set the prompt for the start of line
+	SetPrompt(string)
+
+	// Print a line of output
+	Print(string)
+}
+
+// New create a new REPL and initializes the state machine
+func New(ctx py.Context) *REPL {
+	if ctx == nil {
+		ctx = py.NewContext(py.DefaultContextOpts())
+	}
+
+	r := &REPL{
+		Context:      ctx,
+		prog:         "<stdin>",
+		continuation: false,
+		previous:     "",
+	}
+	r.Module, _ = ctx.ModuleInit(&py.ModuleImpl{
+		Info: py.ModuleInfo{
+			FileDesc: r.prog,
+		},
+	})
+	return r
+}
+
+// SetUI initialises the output user interface
+func (r *REPL) SetUI(term UI) {
+	r.term = term
+	r.term.SetPrompt(NormalPrompt)
+}
+
+// Run runs a single line of the REPL
+func (r *REPL) Run(line string) {
+	// Override the PrintExpr output temporarily
+	oldPrintExpr := vm.PrintExpr
+	vm.PrintExpr = r.term.Print
+	defer func() {
+		vm.PrintExpr = oldPrintExpr
+	}()
+	if r.continuation {
+		if line != "" {
+			r.previous += string(line) + "\n"
+			return
+		}
+	}
+	// need +"\n" because "single" expects \n terminated input
+	toCompile := r.previous + string(line)
+	if toCompile == "" {
+		return
+	}
+	code, err := py.Compile(toCompile+"\n", r.prog, py.SingleMode, 0, true)
+	if err != nil {
+		// Detect that we should start a continuation line
+		// FIXME detect EOF properly!
+		errText := err.Error()
+		if strings.Contains(errText, "unexpected EOF while parsing") || strings.Contains(errText, "EOF while scanning triple-quoted string literal") {
+			stripped := strings.TrimSpace(toCompile)
+			isComment := len(stripped) > 0 && stripped[0] == '#'
+			if !isComment {
+				r.continuation = true
+				r.previous += string(line) + "\n"
+				r.term.SetPrompt(ContinuationPrompt)
+			}
+			return
+		}
+	}
+	r.continuation = false
+	r.term.SetPrompt(NormalPrompt)
+	r.previous = ""
+	if err != nil {
+		r.term.Print(fmt.Sprintf("Compile error: %v", err))
+		return
+	}
+	_, err = r.Context.RunCode(code, r.Module.Globals, r.Module.Globals, nil)
+	if err != nil {
+		py.TracebackDump(err)
+	}
+}
+
+// WordCompleter takes the currently edited line with the cursor
+// position and returns the completion candidates for the partial word
+// to be completed. If the line is "Hello, wo!!!" and the cursor is
+// before the first '!', ("Hello, wo!!!", 9) is passed to the
+// completer which may returns ("Hello, ", {"world", "Word"}, "!!!")
+// to have "Hello, world!!!".
+func (r *REPL) Completer(line string, pos int) (head string, completions []string, tail string) {
+	head = line[:pos]
+	tail = line[pos:]
+	lastSpace := strings.LastIndex(head, " ")
+	head, partial := line[:lastSpace+1], line[lastSpace+1:]
+	// log.Printf("head = %q, partial = %q, tail = %q", head, partial, tail)
+	found := make(map[string]struct{})
+	match := func(d py.StringDict) {
+		for k := range d {
+			if strings.HasPrefix(k, partial) {
+				if _, ok := found[k]; !ok {
+					completions = append(completions, k)
+					found[k] = struct{}{}
+				}
+			}
+		}
+	}
+	match(r.Module.Globals)
+	match(r.Context.Store().Builtins.Globals)
+	sort.Strings(completions)
+	return head, completions, tail
+}
---
+++ repl/repl_test.go
@@ -0,0 +1,133 @@
+package repl
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	// import required modules
+	_ "github.com/jugigibnbniu/gpython/stdlib"
+)
+
+type replTest struct {
+	prompt string
+	out    string
+}
+
+// SetPrompt sets the current terminal prompt
+func (rt *replTest) SetPrompt(prompt string) {
+	rt.prompt = prompt
+}
+
+// Print prints the output
+func (rt *replTest) Print(out string) {
+	rt.out = out
+}
+
+func (rt *replTest) assert(t *testing.T, what, wantPrompt, wantOut string) {
+	t.Helper()
+	if rt.prompt != wantPrompt {
+		t.Errorf("%s: Prompt wrong:\ngot= %q\nwant=%q", what, rt.prompt, wantPrompt)
+	}
+	if rt.out != wantOut {
+		t.Errorf("%s: Output wrong:\ngot= %q\nwant=%q", what, rt.out, wantOut)
+	}
+	rt.out = ""
+}
+
+func TestREPL(t *testing.T) {
+	r := New(nil)
+	rt := &replTest{}
+	r.SetUI(rt)
+
+	rt.assert(t, "init", NormalPrompt, "")
+
+	r.Run("")
+	rt.assert(t, "empty", NormalPrompt, "")
+
+	r.Run("1+2")
+	rt.assert(t, "1+2", NormalPrompt, "3")
+
+	// FIXME this output goes to Stderr and Stdout
+	r.Run("aksfjakf")
+	rt.assert(t, "unbound", NormalPrompt, "")
+
+	r.Run("sum = 0")
+	rt.assert(t, "multi#1", NormalPrompt, "")
+	r.Run("for i in range(10):")
+	rt.assert(t, "multi#2", ContinuationPrompt, "")
+	r.Run("    sum += i")
+	rt.assert(t, "multi#3", ContinuationPrompt, "")
+	r.Run("")
+	rt.assert(t, "multi#4", NormalPrompt, "")
+	r.Run("sum")
+	rt.assert(t, "multi#5", NormalPrompt, "45")
+
+	r.Run("if")
+	rt.assert(t, "compileError", NormalPrompt, "Compile error: \n  File \"<stdin>\", line 1, offset 2\n    if\n\n\nSyntaxError: 'invalid syntax'")
+
+	// test comments in the REPL work properly
+	r.Run("# this is a comment")
+	rt.assert(t, "comment", NormalPrompt, "")
+	r.Run("a = 42")
+	rt.assert(t, "comment continuation", NormalPrompt, "")
+	r.Run("a")
+	rt.assert(t, "comment check", NormalPrompt, "42")
+}
+
+func TestCompleter(t *testing.T) {
+	r := New(nil)
+	rt := &replTest{}
+	r.SetUI(rt)
+
+	for _, test := range []struct {
+		line            string
+		pos             int
+		wantHead        string
+		wantCompletions []string
+		wantTail        string
+	}{
+		{
+			line:            "di",
+			pos:             2,
+			wantHead:        "",
+			wantCompletions: []string{"dict", "divmod"},
+			wantTail:        "",
+		},
+		{
+			line:            "div",
+			pos:             3,
+			wantHead:        "",
+			wantCompletions: []string{"divmod"},
+			wantTail:        "",
+		},
+		{
+			line:            "doodle",
+			pos:             6,
+			wantHead:        "",
+			wantCompletions: nil,
+			wantTail:        "",
+		},
+		{
+			line:            "divmod divm",
+			pos:             9,
+			wantHead:        "divmod ",
+			wantCompletions: []string{"divmod"},
+			wantTail:        "vm",
+		},
+	} {
+		t.Run(fmt.Sprintf("line=%q,pos=%d)", test.line, test.pos), func(t *testing.T) {
+			gotHead, gotCompletions, gotTail := r.Completer(test.line, test.pos)
+			if test.wantHead != gotHead {
+				t.Errorf("invalid head:\ngot= %q\nwant=%q", gotHead, test.wantHead)
+			}
+			if !reflect.DeepEqual(test.wantCompletions, gotCompletions) {
+				t.Errorf("invalid completions:\ngot= %#v\nwant=%#v", gotCompletions, test.wantCompletions)
+			}
+			if test.wantTail != gotTail {
+				t.Errorf("invalid tail:\ngot= %q\nwant=%q", gotTail, test.wantTail)
+			}
+		})
+	}
+
+}
